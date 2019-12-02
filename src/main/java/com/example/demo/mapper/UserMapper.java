0619395ee118a09833a5e84770e119bff816dc50
lianchao <18900945189@163.com>
2019-12-02 19:33:12 +0800

    usermapper  的接口集合

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.example.demo.mapper;
 
 import com.example.demo.bean.User;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +13,12 @@ import java.util.List;
 public interface UserMapper {
     //接口用户存储
     int save(@Param("user") User user);// 新增用户
-    List<User> selectall();
+    List<User> selectall();//  查询所有
     int  deleteuserbyname(@Param("username") String username);// 用户名称
+//    spring boot  修改数据
+//    @Update("update user set username=#{username} where username=#{username}")
+    int update(@Param("user")User user);
+//    @Select("select * from user where id = #{id}")
+//    boolean  findById(@Param("id") String id);
+      List<User> selectbyid(@Param("username") String username);
 }
