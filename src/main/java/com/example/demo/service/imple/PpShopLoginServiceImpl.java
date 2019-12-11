5cdb794908789a4b3accc2a81a3a1f79c891a863
lianchao <18900945189@163.com>
2019-12-11 21:56:18 +0800

    修改登陆的实现接口方法

--- src/main/java/com/example/demo/service/imple/PpShopLoginServiceImpl.java
+++ src/main/java/com/example/demo/service/imple/PpShopLoginServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.demo.service.PpshopLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.mapper.PpshopLoginMapper;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,28 +15,23 @@ import java.util.List;
 public class PpShopLoginServiceImpl implements PpshopLoginService {
     @Autowired
     private PpshopLoginMapper PpshopLoginMapper;
-
-
     /***
      * 该方法栈溢出因为循环调用了
      * @param pShopUser
      * @return
      */
-//    @Override
-//    public String login(PShopUser pShopUser) {
-//
-//        System.out.println(pShopUser.getUsername()+"::::::？？？？？？"+pShopUser.getUserpassword());
-//        String  result=  ppshopLoginService.login(pShopUser);
-//        return result;
-//    }
     @Override
+    @Transactional//  事物注解
     public PShopUser login(PShopUser pShopUser) {
-        List<PShopUser> listps = new ArrayList<PShopUser>();
-        System.out.println(pShopUser.getUsername() + "::::::？？？？？？" + pShopUser.getUserpassword());
+        System.out.println(pShopUser.getUsername() + "::::::" + pShopUser.getUserpassword());
         PShopUser listps1 = PpshopLoginMapper.login(pShopUser);
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!" + listps1);
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!" + listps1.getUserstate() + listps1.getUserphone());
-        return listps1;
+//        if (null != listps1 && "null".equals(listps1)) {
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!" + listps1);
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!" + listps1.getUserstate() + listps1.getUserphone());
+            return listps1;
+//        } else {
+//            return null;
+//        }
     }
 
     /***
@@ -57,7 +53,7 @@ public class PpShopLoginServiceImpl implements PpshopLoginService {
      */
     @Override
     public boolean updatareginuser(PShopUser pShopUser) {
-        boolean  result=PpshopLoginMapper.updatareginuser(pShopUser);
+        boolean result = PpshopLoginMapper.updatareginuser(pShopUser);
         return result;
     }
 
