2ddea350ba32c431bf64f6069de1613a3b84cb38
lianchao <18900945189@163.com>
2020-03-23 10:11:53 +0800

    单挑更新  和查询

--- src/main/java/com/example/demo/service/imple/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/imple/UserServiceImpl.java
@@ -20,7 +20,6 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
-
     /***
      * 存储用
      * @param user
@@ -29,36 +28,33 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional//  事物注解
     public boolean saveUser(User user) {
+        System.out.println(user.getUsername());
+        System.out.println(user.getUsertel());
+        System.out.println(user.getUsertest());
+        System.out.println(user.getUsertype());
+
         boolean result = userMapper.save(user) > 0;
         return result;
     }
 
-    @Override
-    public boolean adduserbyid(int userid) {
-        return false;
-    }
 
     @Override
     @Transactional//  事物注解
     public List<String> selectAll() {
         List<User> list = new ArrayList<User>();
-        List<String> strlist =new ArrayList<String>();
+        List<String> strlist = new ArrayList<String>();
         list.clear();
-     list=userMapper.selectall();
-       Gson gso=new Gson();
-        strlist.add( gso.toJson(list));
-     for (int i=0 ;i<strlist.size();i++ ){
-         System.out.println("长度！！！！！" + strlist.size());
-         System.out.println("长度！！！！！" + strlist.get(i));
-     }
-
+        list = userMapper.selectall();
+        Gson gso = new Gson();
+        strlist.add(gso.toJson(list));
+        for (int i = 0; i < strlist.size(); i++) {
+            System.out.println("长度！！！！！" + strlist.size());
+            System.out.println("长度！！！！！" + strlist.get(i));
+        }
         Log.info("", "" + list.size());
         System.out.println("长度！！！！！" + list.size());
-
-
         return strlist;
     }
-
 
 
     /***
@@ -67,8 +63,49 @@ public class UserServiceImpl implements UserService {
      * @return
      */
     @Override
+    @Transactional//  事物注解
     public boolean deleteuserbyname(String username) {
         boolean result = userMapper.deleteuserbyname(username) > 0;
         return result;
     }
+
+    /**
+     * 通过id进行 单条更新
+     *
+     * @param user
+     * @return
+     */
+
+
+    @Override
+    public boolean update(User user) {
+        if (null != user.getUsername()) {
+            System.out.println("updataResult======impl" + user.getUsername());
+            System.out.println("getUsertype=====impl" + user.getUsertype());
+            System.out.println("getUsertest======impl" + user.getUsertest());
+            System.out.println("getUsertel======impl" + user.getUsertel());
+            boolean result = userMapper.update(user) > 0;
+            return result;
+        }
+        return false;
+    }
+
+
+    /***
+     * t通过用户的id查询单条记录
+     * @param username
+     * @return
+     */
+    @Transactional//  事物注解
+    @Override
+    public List<String> selectbyid(String username) {
+        List<User> list = new ArrayList<User>();
+        List<String> strlist = new ArrayList<String>();
+        list = userMapper.selectbyid(username);
+        Gson gso = new Gson();
+        strlist.add(gso.toJson(list));
+        return strlist;
+    }
+
+
 }
