5f4c29d696adcc995c6a8ef3e7c8a6ff29e2cc21
lianchao <18900945189@163.com>
2020-04-29 16:47:08 +0800

    家庭类的返回和分页

--- src/main/java/com/example/demo/controller/FamilyController.java
+++ src/main/java/com/example/demo/controller/FamilyController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.bean.ppshopbean.ppshopfamilybean;
+import com.example.demo.domain.GeneralResult;
 import com.example.demo.service.FamilyService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,30 +36,50 @@ public class FamilyController {
             @ApiResponse(code = 400, message = "请求参数没填好"),
             @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
     })
-    private String GetAllFamilyResult(String username, String Cardid) {
-        System.out.println("----" + username + "--------Cardid--" + Cardid);
+    private GeneralResult GetAllFamilyResult(String username, String Cardid) {
+        GeneralResult General = new GeneralResult();
+        System.out.println("username----" + username + "--------Cardid--" + Cardid);
         List<ppshopfamilybean> ppsfbeanlist = new ArrayList<ppshopfamilybean>();
         if (null == username || username.equals("")) {
             System.out.println("用户名不能为空");
-            return "用户名不能为空";
+            General.setMsg("用户名不能为空");
+            General.setCode(99);
+            return General;
         } else {
             if (null == Cardid || Cardid.equals("")) {
                 System.out.println("身份证号码不能是空");
-                return "身份证号码不能是空";
+                General.setMsg("身份证号码不能是空");
+                General.setCode(99);
+                return General;
+
             } else {
-                System.out.println(username + "-----Cardid---"+Cardid);
-//                familybean f =new familybean();
-//                f.setCardid(Cardid);
-//                f.setName(username);
+                System.out.println("----username==="+username + "-----Cardid---"+Cardid);
                 ppshopfamilybean  ppshopfamily=new ppshopfamilybean();
                 ppshopfamily.setCardid(Cardid);
                 ppshopfamily.setName(username);
+
                 ppsfbeanlist = familyService.Getfamilybean(ppshopfamily);
+
+                General.setMsg("Success");
+                General.setCode(00);
+                General.setData(ppshopfamily);
                 for (int i = 0; i < ppsfbeanlist.size(); i++) {
                     System.out.println(ppsfbeanlist.get(i) + "--------");
                 }
-                return ppsfbeanlist.toString();
+                return General;
             }
         }
     }
+
+
+    /*****
+     * 分页显  和逐条返回
+     */
+
+    public    GeneralResult  selectReturnData(String paper,String papershow){
+        GeneralResult  generalResult=new GeneralResult();;
+
+        return generalResult;
+    }
+
 }
