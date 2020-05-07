40d7ef0daaed050b873df96a963d56e1682b8a2c
lianchao <18900945189@163.com>
2020-05-07 14:24:56 +0800

    实现分页代码 每个显示条数页数和当前页数

---
+++ src/main/java/com/example/demo/bean/ppshopbean/Lay.java
@@ -0,0 +1,21 @@
+package com.example.demo.bean.ppshopbean;
+
+/***
+ * 分页类的实现
+ */
+public class Lay {
+    private Integer limit;//每页显示条数
+
+    private Integer page;//当前页数
+
+    private int code;
+
+    private String msg;
+
+    private Long count;//返回数据总条数
+
+    private Object data;
+//————————————————
+//    版权声明：本文为CSDN博主「何必讨好女人」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+//    原文链接：https://blog.csdn.net/qq_41968670/java/article/details/83006676
+}
