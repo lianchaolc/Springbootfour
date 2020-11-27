2ff2c13cf256b5e4305f03b631d95b88c6675166
lianchao <18900945189@163.com>
2020-11-27 21:14:44 +0800

    测试类无实际以目的调用helloserv   的方法

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.swing.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -12,5 +14,8 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
+@Test
+	public   void  testHelloService(){
+//		boolean   b= Icon.constainbean("helloService")
+}
 }
