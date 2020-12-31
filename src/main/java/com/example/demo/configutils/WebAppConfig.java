222461e0c15cac028a3da59b10a2fb1d8ca61cab
“lianchaolc” <15642926631@163.com>
2020-12-31 14:19:31 +0800

    提交代码

---
+++ src/main/java/com/example/demo/configutils/WebAppConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.configutils;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * spring  boot 保存图片
+ * https://blog.csdn.net/qq_43021380/article/details/93509686
+ *https://blog.csdn.net/weixin_41496975/article/details/81281030
+ */
+@RestController
+@RequestMapping("upload")
+public class WebAppConfig    {
+
+
+}
